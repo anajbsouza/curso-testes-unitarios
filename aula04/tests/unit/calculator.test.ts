2ab3125055a92e714b1c17e7ede4fc0a6d741c03
anajbsouza <anaju.batsouza@gmail.com>
2023-10-03 17:51:03 -0300

    ex 4

--- aula04/tests/unit/calculator.test.ts
+++ aula04/tests/unit/calculator.test.ts
@@ -1,22 +1,20 @@
+import calculator from "../../src/calculator";
+
 describe("calculator tests", () => {
   it("should return a sum", async () => {
-    const a = 5;
-    const b = 10;
-    expect(a+b).toBe(15);
+    const value = calculator.sum(5,10)
+    expect(value).toBe(15);
   });
   it("should return a sub", async () => {
-    const a = 5;
-    const b = 10;
-    expect(b-a).toBe(5);
+    const value = calculator.sub(10,5)
+    expect(value).toBe(5);
   });
-  it("should return a mult", async () => {
-    const a = 5;
-    const b = 10;
-    expect(a*b).toBe(50);
+  it("should return a mul", async () => {
+    const value = calculator.mul(5,10)
+    expect(value).toBe(50);
   });
   it("should return a div", async () => {
-    const a = 5;
-    const b = 10;
-    expect(b/a).toBe(2);
+    const value = calculator.div(10,2)
+    expect(value).toBe(5);
   });
 })
